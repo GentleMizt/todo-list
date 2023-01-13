0901b2ba1ebf100a564e9fd3155976fa2e920316
GentleMizt <olamideiyanda18@gmail.com>
2023-01-13 15:48:58 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,25 @@
 let inp = document.querySelector('#inp');
 let inp_btn = document.querySelector('#push');
+let todo = document.querySelector('#tasks');
 
 // console.log(inp.value);
-inp.addEventListener('input', ()=>{
-    console.log(inp.value);
-})
+document.addEventListener('keypress', (e)=>{
+    if (e.keyCode === 13) {
+        getTask();
+    }
+});
+inp_btn.addEventListener('click',getTask)
+
+
+function getTask (){ 
+   if (inp.value !== '') {
+        todo.innerHTML += 
+        `<div class="task">
+            <span>${inp.value}</span>         
+            <button>Delete</button>
+        </div>`
+        inp.value = ''
+    } else {
+    alert('Input field cannot be empty');
+}
+}
