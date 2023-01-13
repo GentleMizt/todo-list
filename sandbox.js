9771505fb074aa8c6cb898ca8e061be908048a4a
GentleMizt <olamideiyanda18@gmail.com>
2023-01-13 17:06:04 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,6 @@
 let inp = document.querySelector('#inp');
 let inp_btn = document.querySelector('#push');
 let todo = document.querySelector('#tasks');
-
 // console.log(inp.value);
 document.addEventListener('keypress', (e)=>{
     if (e.keyCode === 13) {
@@ -16,10 +15,25 @@ function getTask (){
         todo.innerHTML += 
         `<div class="task">
             <span>${inp.value}</span>         
-            <button>Delete</button>
+            <button onclick="dlt(this)" class="Del">Delete</button>
         </div>`
         inp.value = ''
     } else {
     alert('Input field cannot be empty');
 }
 }
+
+//      LEYE'S METHOD
+    function dlt(e) {
+        e.parentElement.remove();
+    }
+
+
+
+
+//      SAMUEL'S METHOD!
+// todo.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('Del')) {
+//        e.target.parentNode.remove();
+//     }
+//    });
