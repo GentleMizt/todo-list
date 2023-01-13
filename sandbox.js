c891b75e227d980e5b7e9afdf8c48aee17ae9499
GentleMizt <olamideiyanda18@gmail.com>
2023-01-13 14:46:32 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,7 @@
 let inp = document.querySelector('#inp');
-let inp_btn = document.querySelector('#push');d
+let inp_btn = document.querySelector('#push');
+
+// console.log(inp.value);
+inp.addEventListener('input', ()=>{
+    console.log(inp.value);
+})
