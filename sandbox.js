8c12111f9257e055c708127202cbd75fd12f161e
GentleMizt <olamideiyanda18@gmail.com>
2023-01-16 14:29:05 +0100

    progress

--- sandbox.js
+++ sandbox.js
@@ -49,52 +49,14 @@ function getTask (){
     inp.value = ''  ;
 }
 
-
-        //  SAMUEL'S METHOD!
     todo.addEventListener('click', (e) => {
         if (e.target.classList.contains('Del')) {
-            // console.log(e.target);
-            // inp.value = span.textContent  ;
            e.target.parentNode.parentNode.remove();
         }
     });
     todo.addEventListener('click', (e) => {
         if (e.target.classList.contains('edit')) {
-            // console.log(e.target);
             inp.value = e.target.parentNode.parentNode.children[0].textContent;
-            // console.log(e.target.parentNode.parentNode.children[0].textContent);
            e.target.parentNode.parentNode.remove();
         }
     });
-
-    // let val = document.querySelector('.span-value');
-    // console.log(val.textContent);
-    // function list () {
-    
-    // }
-
-    // list();
-
-
-
-
-
-
-    // if (input.value !== "") {
-    //     for (let i = 0; i < listItems.length; i++) {
-    //       console.log(listItems[i].innerText);
-    //       if (listItems[i].innerText === input.value) {
-    //         err.innerText =
-    //           "Item already exists click on the item to increase its quantity";
-    //         input.value = "";
-    //         return;
-    //       }
-    //     }
-    //     const li = document.createElement("li");
-    //     li.innerText = input.value;
-    //     ul.appendChild(li);
-        
-    //     console.log(listItems);
-    //     input.value = "";
-    //   }
-    //   console.log(ul.children
